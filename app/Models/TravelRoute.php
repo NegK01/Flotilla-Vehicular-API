d230dd0cb02071d97934b5d865eeaea4fe6cdf7a
NegK01 <mauricio.or99@gmail.com>
2026-03-22 13:47:50 -0600

    feature(travelRoutes): add travelRoute model and migration

---
+++ app/Models/TravelRoute.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TravelRoute extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'travel_routes';
+
+    protected $fillable = [
+        'name',
+        'start_point',
+        'end_point',
+        'estimated_distance',
+        'description',
+    ];
+
+    protected $casts = [
+        'name' => 'string',
+        'start_point' => 'string',
+        'end_point' => 'string',
+        'estimated_distance' => 'decimal:2',
+        'description' => 'string',
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'id';
+    }
+}
