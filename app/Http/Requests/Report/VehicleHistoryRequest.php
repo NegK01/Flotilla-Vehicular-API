3a2adf0fcabe43d17114734d9508b354faa2f6a5
Lucas Lopez Rodriguez <lucaslr0106@gmail.com>
2026-04-26 22:04:22 -0400

    feat(report): add VehicleHstoryReport

--- app/Http/Requests/Report/VehicleHistoryRequest.php
+++ app/Http/Requests/Report/VehicleHistoryRequest.php
@@ -2,28 +2,20 @@
 
 namespace App\Http\Requests\Report;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class VehicleHistoryRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date', 'after_or_equal:start_date'],
         ];
     }
 }
